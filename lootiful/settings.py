c0c395d988a6d5d1196d062e85bbf3938e83b00b
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-03-07 12:40:24 +0000

    created cart app, adjust_cart view

--- lootiful/settings.py
+++ lootiful/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
     'accounts',
-    'products'
+    'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
