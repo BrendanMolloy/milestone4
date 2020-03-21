47579fbb66536e7b88cd3fe0f8e199aa9367928a
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-03-21 11:58:01 +0000

    changed project name from Lootiful to Lootify, including wherever Lootiful is mentioned

--- lootify/wsgi.py
+++ lootify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lootiful project.
+WSGI config for lootify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lootiful.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lootify.settings")
 
 application = get_wsgi_application()
