a8ef9c9d504a751ff7035702c31c0271580fcb68
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-01 12:54:51 +0000

    renamed product.html tp productdetail.html, created the product_detail view, and added product_detail to urls.py

--- products/views.py
+++ products/views.py
@@ -5,3 +5,15 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
+
+def product_detail(request, pk):
+    """
+    Create a view that returns a single
+    Product object based on the product ID (pk) and
+    render it to the 'productdetail.html' template.
+    Or return a 404 error if the product is
+    not found
+    """
+    product = get_object_or_404(Product, pk=pk)
+    product.save()
+    return render(request, "producttdetail.html", {'product': product})
