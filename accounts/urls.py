381d1ad0775db8a6fa6bebedc4d528c9489e9291
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-09 15:52:33 +0000

    profile.html no longer specifies profile id in te link to edit profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import index, register, profile, logout, login, edit_profile
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
-    url(r'^(?P<pk>\\d+)/edit/$', edit_profile, name='edit_profile'),
+    url(r'^profile/edit/$', edit_profile, name='edit_profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
     url(r'^password-reset/', include(urls_reset)),
