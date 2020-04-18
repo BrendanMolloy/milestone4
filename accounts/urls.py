3c9506957cbb317d548f8d1084a7d679660263db
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-18 13:02:50 +0000

    updated accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from .views import index, register, profile, logout, login, edit_profile
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
-    url(r'^profile/$', profile, name='profile'),
+    url(r'^profile/(?P<pk>\d+)/$', profile, name='profile'),
     url(r'^profile/edit/$', edit_profile, name='edit_profile'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
