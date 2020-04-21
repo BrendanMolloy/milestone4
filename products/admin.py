5d902397f8e030d48c90b1e3f58d8fc6f7d31cf1
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-21 13:32:39 +0000

    imported Comment model to products/admin.py again

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,7 @@ from .models import Product, Comment
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Comment)
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'body', 'product', 'created_on', 'active')
