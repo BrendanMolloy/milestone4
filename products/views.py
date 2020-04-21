830376659037e1a50fcfabf0b249afbb0b4fae99
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-21 13:51:03 +0000

    added comments to the product_detail view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .forms import CommentForm
 
 # Create your views here.
 def all_products(request):
@@ -16,4 +17,24 @@ def product_detail(request, pk):
     """
     product = get_object_or_404(Product, pk=pk)
     product.save()
-    return render(request, "productdetail.html", {'product': product})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, "productdetail.html", {'product': product, 
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
