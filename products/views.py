a833b89480453c314ca27f4e0afd3b3d99504fee
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-23 16:19:58 +0000

    trying to pull user.username 8

--- products/views.py
+++ products/views.py
@@ -18,7 +18,8 @@ def product_detail(request, pk):
     """
     product = get_object_or_404(Product, pk=pk)
     user_id = request.user.pk 
-    current_user = User.username(user=user_id)
+    current_user = User.objects.get(user=user_id)
+    current_username = current_user.username
     
     comments = product.comments.filter(active=True)
     new_comment = None
@@ -31,7 +32,7 @@ def product_detail(request, pk):
             new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
             new_comment.product = product
-            new_comment.user = current_user
+            new_comment.user = current_username
             # Save the comment to the database
             new_comment.save()
     else:
