e233e924049d7945e588c9d443f798f1fa4e9cbb
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-04-23 14:35:25 +0000

    removed user_id from Comment model, reverted to user

--- products/models.py
+++ products/models.py
@@ -14,7 +14,6 @@ class Product(models.Model):
 class Comment(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE,related_name='comments')
     user = models.OneToOneField(User)
-    user_id = models.OneToOneField(User.username)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
@@ -23,4 +22,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.body, self.user_id)
+        return 'Comment {} by {}'.format(self.body, self.user)
