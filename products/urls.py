01b6cdf673f57e7e0ba52acec14faaecbfe14e40
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-13 20:19:14 +0000

    imported homepage view to products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail
+from .views import all_products, product_detail, homepage
 
 urlpatterns = [
     url(r'^$', homepage, name='homepage'),
-    url(r'^all_products/$', all_products, name='products'),
+    url(r'^all_products/', all_products, name='products'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail')
 ]
