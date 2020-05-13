5463f60c6fd31cdba6a1568b368ce6b3f3bf5d57
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-13 20:10:24 +0000

    imported homepage to lootify.urls

--- lootify/urls.py
+++ lootify/urls.py
@@ -21,6 +21,7 @@ from cart import urls as urls_cart
 from search import urls as urls_search
 from checkout import urls as urls_checkout
 from products.views import all_products
+from products.views import homepage
 from django.views import static
 from .settings import MEDIA_ROOT
 
