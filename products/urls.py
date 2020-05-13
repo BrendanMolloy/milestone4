67bc16acf6ce9e856ccecf4b3f784819e56a5ba0
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-13 20:16:15 +0000

    updated products/urls.py to include the homepage view and update all_products to have its own url pattern

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from .views import all_products, product_detail
 
 urlpatterns = [
-    url(r'^$', all_products, name='products'),
+    url(r'^$', homepage, name='homepage'),
+    url(r'^all_products/$', all_products, name='products'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail')
 ]
