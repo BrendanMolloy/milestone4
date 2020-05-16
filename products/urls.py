a1333432763677e68bb8eeda928065d3aa35a059
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-16 18:13:38 +0000

    removed the filter views and associated url, removed the a tag from index.html

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,5 @@ from .views import all_products, product_detail, products_weapon, products_armor
 
 urlpatterns = [
     url(r'^$', all_products, name='all_products'),
-    url(r'^weapons/$', products_weapon, name='all_weapons'),
-    url(r'^armor/$', products_armor, name='all_armor'),
-    url(r'^accessories/$', products_accessory, name='all_accessories'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail')
 ]
