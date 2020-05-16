f26d645c4ff80739e0da4e82b99db0069f993af9
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-16 16:33:00 +0000

    created urls for each of the new views

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail
+from .views import all_products, product_detail, products_weapon, products_armor, products_accessory
 
 urlpatterns = [
     url(r'^$', all_products, name='all_products'),
+    url(r'^weapons/$', products_weapon, name='all_weapons'),
+    url(r'^armor/$', products_armor, name='all_armor'),
+    url(r'^accessories/$', products_accessory, name='all_accessories'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail')
 ]
