3aa64a739ed5d2a7850cd7052d45f0a5e68feafb
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-16 18:19:46 +0000

    unimported the filter views to products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail, products_weapon, products_armor, products_accessory
+from .views import all_products, product_detail
 
 urlpatterns = [
     url(r'^$', all_products, name='all_products'),
