1d38de6c33b73eb73ba0e4a900ebe64fa748d1ad
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-19 15:30:06 +0000

    imported login_required to products/views.py

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Comment
 from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 # Create your views here.
