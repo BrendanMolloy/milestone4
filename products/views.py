8ad8c2c4f621de0ca3988aefb112e31bb36d9d0b
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-21 15:17:13 +0000

    trying to pull comments by their id part 48, imported redirect

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Comment
 from .forms import CommentForm
 from django.core.urlresolvers import reverse, reverse_lazy
