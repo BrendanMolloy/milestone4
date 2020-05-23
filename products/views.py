88d7640a6458ad99445b16af66026df2060e70a5
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-23 15:02:11 +0000

    clearing the comment form after submitting a new comment

--- products/views.py
+++ products/views.py
@@ -56,6 +56,7 @@ def product_detail(request, id):
             comment_form = CommentForm()
 
         product.save()
+        new_comment.clear()
         return render(request, "productdetail.html", {'product': product, 
                                             'comments': comments,
                                             'new_comment': new_comment,
