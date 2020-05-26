2f4dcb1a75622e227a86ebce8d658d91a40d815b
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-26 17:52:27 +0000

    removed crunchy, used custom css to restyle comment form

--- lootify/settings.py
+++ lootify/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
-    'crispy_forms',
 #     'storages'
 ]
 
@@ -166,5 +165,3 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
