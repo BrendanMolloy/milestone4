7c8a7a290fa0824a22702c126c065beb6b7d667d
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-27 14:48:28 +0000

    add_to_cart refreshes page after incorrect addition part 2

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 # Create your views here.
@@ -18,7 +19,7 @@ def add_to_cart(request, id):
         return redirect(reverse('index'))
     except ValueError:
         messages.error(request, "Please correct the highlighted errors:")
-        return redirect('../')
+        return HttpResponseRedirect("")
 
 
 def adjust_cart(request, id):
