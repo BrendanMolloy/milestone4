2b59eefebb3f0517229ed186a418ce76fa8ae54a
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-27 13:20:24 +0000

    separated the change_email_or_password view from the edit_profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url, include
 from . import urls_reset
-from .views import index, register, profile, logout, login, edit_profile
+from .views import index, register, profile, logout, login, edit_profile, change_email_or_password
 
 urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
     url(r'^profile/edit/$', edit_profile, name='edit_profile'),
-    url(r'^profile/change_password_or_email/$', edit_profile, name='change_password_or_email'),
+    url(r'^profile/change_password_or_email/$', change_email_or_password, name='change_password_or_email'),
     url(r'^logout/$', logout, name='logout'),
     url(r'^login/$', login, name='login'),
     url(r'^password-reset/', include(urls_reset)),
