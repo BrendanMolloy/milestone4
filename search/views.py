1f8ead72c94a5adcc43116d13a69b71d91fbc799
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-30 18:39:40 +0000

    search bar now works with pagination

--- search/views.py
+++ search/views.py
@@ -3,5 +3,14 @@ from products.models import Product
 
 # Create your views here.
 def do_search(request):
-    products = Product.objects.filter(name__icontains=request.GET['q'])
+    products_list = Product.objects.filter(name__icontains=request.GET['q'])
+    paginator = Paginator(products_list, 9) # limits number of products to 9 per page
+    try:
+        page = int(request.GET.get('page','1'))
+    except:
+        page = 1
+    try:
+        products = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
     return render(request, "products.html", {"products": products})
---
+++ test_cases.md
@@ -0,0 +1,39 @@
+# Test Cases
+
+## Nav
+Searchbar
+Links
+Cart
+Collapse
+
+## Index
+Carousel
+Category Icons
+
+## All Products
+Links
+Forms
+
+## Product Details
+Form
+Comment
+Edit Comment
+Delete Comment
+
+## Cart
+Form
+Link
+
+## Checkout
+Order Form
+Payment Form
+
+## Profile
+Edit Profile
+Email Update
+Password Update
+Delete Profile
+
+## Orders
+
+
