06e0fa97d22f5277ddf60590472f271fc18825b6
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-30 11:58:55 +0000

    debugging orders page part 17

--- accounts/views.py
+++ accounts/views.py
@@ -158,5 +158,5 @@ def delete_profile(request):
 def orders(request):
     """A view that displays the orders page"""
     orders = Order.objects.all().order_by('date')
-    order_line_items = OrderLineItem.objects.all ().order_by('date')
+    order_line_items = OrderLineItem.objects.all().order_by('order')
     return render(request, "orders.html", {'orders': orders, 'order_line_items': order_line_items})
