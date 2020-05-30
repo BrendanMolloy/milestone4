563c862ba4b69b7f31e389a44bf017ca865f866a
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-30 07:33:37 +0000

    checkout/views.py bug fixing part 2

--- checkout/views.py
+++ checkout/views.py
@@ -38,50 +38,51 @@ def checkout(request):
                                             'street_address2': currentprofile.street_address2,
                                             'county': currentprofile.county})
     except Profile.DoesNotExist:    
-        if request.method == "POST":
-            order_form = OrderForm(request.POST)
-            payment_form = MakePaymentForm(request.POST)
+        order_form = order_form
+    if request.method == "POST":
+        order_form = OrderForm(request.POST)
+        payment_form = MakePaymentForm(request.POST)
 
-            if order_form.is_valid() and payment_form.is_valid():
-                order = order_form.save(commit=False)
-                order.date = timezone.now()
-                order.save()
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
 
-                cart = request.session.get('cart', {})
-                total = 0
-                for id, quantity in cart.items():
-                    product = get_object_or_404(Product, pk=id)
-                    total += quantity * product.price
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        product=product,
-                        quantity=quantity
-                    )
-                    order_line_item.save()
-                
-                try:
-                    customer = stripe.Charge.create(
-                        amount=int(total * 100),
-                        currency="EUR",
-                        description=request.user.email,
-                        card=payment_form.cleaned_data['stripe_id']
-                    )
-                # Provides various messages to user dependent on success of order
-                except stripe.error.CardError:
-                    messages.error(request, "Your card was declined!")
-                
-                if customer.paid:
-                    messages.error(request, "You have successfully paid")
-                    request.session['cart'] = {}
-                    return redirect(reverse('index'))
-                else:
-                    messages.error(request, "Unable to take payment")
+            cart = request.session.get('cart', {})
+            total = 0
+            for id, quantity in cart.items():
+                product = get_object_or_404(Product, pk=id)
+                total += quantity * product.price
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity
+                )
+                order_line_item.save()
+            
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100),
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id']
+                )
+            # Provides various messages to user dependent on success of order
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('index'))
             else:
-                print(payment_form.errors)
-                messages.error(request, "We were unable to take a payment with that card!")
+                messages.error(request, "Unable to take payment")
         else:
-            payment_form = MakePaymentForm()
-            order_form = OrderForm()
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm()
+        order_form = OrderForm()
         
         # auto-fills name and address information if those details have been completed on Profile page
     return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
