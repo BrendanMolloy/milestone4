72d930a39a0e9156dd9982676532773b1b224ad1
BrendanMolloy <brendanmolloyonline@gmail.com>
2020-05-31 13:26:36 +0000

    return an error message if amending quanitity by null value in cart.html

--- cart/views.py
+++ cart/views.py
@@ -26,13 +26,17 @@ def adjust_cart(request, id):
     Adjust the quantity of the specified product to the specified
     amount
     """
-    quantity = int(request.POST.get('quantity'))
-    cart = request.session.get('cart', {})
+    try:
+        quantity = int(request.POST.get('quantity'))
+        cart = request.session.get('cart', {})
 
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
-    
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
+        if quantity > 0:
+            cart[id] = quantity
+        else:
+            cart.pop(id)
+        
+        request.session['cart'] = cart
+        return redirect(reverse('view_cart'))
+    except ValueError:
+        messages.error(request, "Cannot add a null value quantity to the cart")
+        return redirect(request.META['HTTP_REFERER'])
